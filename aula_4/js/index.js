989a00168ded340e38f3ecc309baeede612b6e19
marcossvaz <marcos.si.vaz@gmail.com>
2023-01-17 23:26:01 -0300

    aula 6

---
+++ aula_4/js/index.js
@@ -0,0 +1,12 @@
+
+var nome = "marcos";
+
+console.log(nome);
+
+var nome = "Carlos";   //Tipagem fraca possibilita a mudança da variavel
+
+console.log(nome);     
+
+    nome = 2;
+
+    console.log(nome);
