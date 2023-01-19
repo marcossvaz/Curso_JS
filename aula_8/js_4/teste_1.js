ce583c495bf975a09bc09b6dfc7b9e9c491468d4
marcossvaz <marcos.si.vaz@gmail.com>
2023-01-19 13:40:34 -0300

    aula de objeto e arrys

---
+++ aula_8/js_4/teste_1.js
@@ -0,0 +1,12 @@
+var objProf ={
+    profissao1: "proramador",
+    profissao2: "agente",
+    profissao3: "entregador",
+};
+
+console.log(objProf.profissao1);
+
+objProf.profissao1 = "programador";
+
+console.log(objProf.profissao1);
+
---
+++ aula_9/js/teste_9.js
@@ -0,0 +1,12 @@
+var carros = ["marcos", "silva", "vaz", "nascimento"];
+
+console.log( typeof carros);
+console.log(carros);
+
+ carros[3] = "Quero";
+
+ console.log(carros);
+
+ carros[5] = "Nascimento";
+
+ console.log(carros);
