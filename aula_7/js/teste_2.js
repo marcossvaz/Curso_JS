b22f60ced94b4b0bd0fe1402296a6aa24a85eef8
marcossvaz <marcos.si.vaz@gmail.com>
2023-01-20 08:07:21 -0300

    teste de objetos

---
+++ aula_7/js/teste_2.js
@@ -0,0 +1,13 @@
+var cadastro = {
+    nome: "marcos",
+    tel: 3242342343,
+    idade: 23,
+    bairro: "alto do peru",
+}
+
+console.log(cadastro);
+
+cadastro.nome = "Marcos";
+
+console.log(cadastro.nome);
+console.log(cadastro);
