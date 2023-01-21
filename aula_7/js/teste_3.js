2c3edc57389714e18fdafeae8ba899fa57cd125d
marcossvaz <marcos.si.vaz@gmail.com>
2023-01-21 08:30:38 -0300

    teste

---
+++ aula_7/js/teste_3.js
@@ -0,0 +1,9 @@
+var cadastro = ["marcos", "carlinhos"];
+
+console.log(cadastro);
+
+console.log(cadastro[1]);
+
+cadastro[2] = "klaudia";
+
+console.log(cadastro);
---
+++ aula_7/js/teste_4.js
@@ -0,0 +1,16 @@
+var situacao = {
+    sit_1: "disponível",
+    sit_2: "ausente",
+    sit_3: "carregando",
+}
+
+console.log(situacao.sit_1);
+console.log(situacao);
+
+var marcas = {
+    mar_1: "volks",
+    mar_2: "audi",
+    mar_3: "bmw",
+}
+
+console.log(marcas.mar_2);
