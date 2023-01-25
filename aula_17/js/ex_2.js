ae86793c4e68bc9efe7c073db5277706f4160c67
marcossvaz <marcos.si.vaz@gmail.com>
2023-01-25 12:16:33 -0300

    teste

--- aula_17/js/ex_2.js
+++ aula_17/js/ex_2.js
@@ -1,6 +1,16 @@
-var n = 1;
-var nome = "marcos";
-while(n < 10){
-    console.log(`${nome}`);
-    n += 1;
-}
+ var idade = 21;
+ var num = 20;
+
+ console.log(idade)
+
+ idade += num;
+
+
+ 
+ console.log(idade);
+
+ var a 
+a 
+=
+3
+console.log(a)
---
+++ aula_18/js/ex_2.js
@@ -0,0 +1,7 @@
+var carros = ["f12", "galardo", "bugatti"];
+
+for( n = 0; n < carros.length; n++){
+    console.log(carros[n]);
+}
+
+
