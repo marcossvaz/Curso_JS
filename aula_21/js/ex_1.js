3c8f1660f98a17fe93c2df1fb8731e037945ce49
marcossvaz <marcos.si.vaz@gmail.com>
2023-01-27 13:50:16 -0300

    aula de escopo

--- aula_21/js/ex_1.js
+++ aula_21/js/ex_1.js
@@ -1,8 +1,7 @@
 let idade = 20;
 
-if(idade <= 19){
+if(idade < 19){
     console.log(`Olá você não pode acessar esse brinquedo`);
 }else {
     console.log(`pode entrar`);
 }
-
---
+++ aula_21/js/ex_2.js
@@ -0,0 +1,8 @@
+
+function f(){
+    var escopo = "Que isso menino";
+    console.log(escopo);
+    
+}
+
+f()
