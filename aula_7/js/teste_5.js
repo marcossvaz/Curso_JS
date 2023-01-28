2b8a606cacb0f869b57cc382dfe7e2821b514f4e
marcossvaz <marcos.si.vaz@gmail.com>
2023-01-28 18:56:48 -0300

    aula de objetos

---
+++ aula_7/js/teste_5.js
@@ -0,0 +1,12 @@
+var livros= {
+    livro1: "pequeno príncipe",
+    livro2: "senhor dos anéis",
+    livro3: "As duas torres",
+    livro4: "",
+}
+
+console.log(livros.livro1);
+
+livros.livro4 = "sociedade do anel";
+
+console.log(livros);
