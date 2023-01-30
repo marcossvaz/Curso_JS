e7feb85e3f0c900cfab9cee38c0d69036295992c
marcossvaz <marcos.si.vaz@gmail.com>
2023-01-30 11:11:29 -0300

    prática de arrys

---
+++ aula_8/js_4/teste_2.js
@@ -0,0 +1,11 @@
+var carros = [["volks", {1:"gol", 2:"golf"}], "fiat", "audi", "GM"];
+
+carros.push("ferrari");
+
+if(carros[0] == "volks"){
+    console.log(`Quais os modelos da ${carros[0]} que você tem ?`);
+} else {
+    console.log(`quais os modelos que você tem? modelos: ${carros}`);
+}
+
+
