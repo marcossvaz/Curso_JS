76e2ad6c6b10541f0e480e8378cfc7d362ec1d31
marcossvaz <marcos.si.vaz@gmail.com>
2023-02-01 12:36:25 -0300

    enviado

---
+++ aula_20/js/ex_2.js
@@ -0,0 +1,8 @@
+var nome = "marcos";
+var idade = 20;
+var identificacao = 232323 ;
+
+function cadastroCartao(nome, idade, identificacao){
+    console.log(`Seu nome é ${nome}, Sua idade é ${idade} e Seu RG ${identificacao}`);
+}
+cadastroCartao(nome, idade, identificacao);
