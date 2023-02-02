a04e3f6ed26518305b1172ae75e15ff8cceb40ea
marcossvaz <marcos.si.vaz@gmail.com>
2023-02-02 15:30:42 -0300

    enviar

---
+++ aula_25/js/ex_1.js
@@ -0,0 +1,8 @@
+var nome = ["ferrari", "fiat", "fusca", "bugatti"];
+
+console.log(nome.length);
+console.log(nome.push(5));
+console.log(nome.pop(5));
+console.log(nome.unshift("marquinhos"));
+
+console.log(nome);
