8995ea08adacaa835c318bcb9f03ae4268371b68
marcossvaz <marcos.si.vaz@gmail.com>
2023-02-01 21:36:16 -0300

    enviado

---
+++ aula_22/js/ex_1.js
@@ -0,0 +1,8 @@
+console.log(parseFloat("20.25424"));
+
+console.log(parseInt("23.4343")) //Transforma número em inteiro 
+
+console.log(23.345.toFixed(2)); //limita a casa Decimal no número
+
+console.log(isNaN(32));
+console.log(isNaN("marcos"));
---
+++ aula_23/js/ex_1.js
@@ -0,0 +1,7 @@
+var nome = "carro é uma meio de locomoção";
+console.log(nome);
+console.log(nome.indexOf("carro"));
+
+var novoMeio = nome.replace("uma", "um");
+
+console.log(novoMeio);
