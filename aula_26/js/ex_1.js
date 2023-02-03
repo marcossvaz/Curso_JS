5b3506d76874dd7b94eeebd347a27d6a7d0a50a1
marcossvaz <marcos.si.vaz@gmail.com>
2023-02-03 13:00:34 -0300

    enviado

---
+++ aula_26/js/ex_1.js
@@ -0,0 +1,7 @@
+var modelos = ["ferrari", "bugatti", "fiat"];
+
+console.log(modelos);
+
+console.log(modelos.splice(1,0,"gm"));
+
+console.log(modelos);
---
+++ aula_27/js/ex_1.js
@@ -0,0 +1,10 @@
+var cadastro = {
+    nome: "marcos",
+    idade: 23,
+    falaFav: function fala(){
+        console.log("meu nome é marcos");
+    }
+
+}
+
+cadastro.falaFav();
