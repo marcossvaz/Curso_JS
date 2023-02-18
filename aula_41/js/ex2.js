5a05258c10869f127c7c30d240ad2b013aa580c8
marcossvaz <marcos.si.vaz@gmail.com>
2023-02-18 15:11:37 -0300

    feito

---
+++ aula_41/js/ex2.js
@@ -0,0 +1,6 @@
+/*var at1 = document.querySelector(".t1");
+at1.setAttribute("class", 'titulo1');
+console.log(at1); */ /*trocando elemento*/
+
+var trocaEstilo = document.querySelector(".t1");
+trocaEstilo.style.color = "red;"
