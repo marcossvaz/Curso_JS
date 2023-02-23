0b3913ee83bd765a1d619bc80ada9a3c522571c3
marcossvaz <marcos.si.vaz@gmail.com>
2023-02-23 14:00:27 -0300

    enviado

---
+++ aula10/js.js
@@ -0,0 +1,7 @@
+let  usuario= "marcos";
+
+if( usuario === "marcos"){
+  console.log("olá "+usuario);
+} else {
+  console.log("Não é "+usuario);
+}
---
+++ aula9/js.js
@@ -0,0 +1,6 @@
+let hab1 = ["corrida", "resistência", "força"];
+let hab2 = ["destreza", "persuassão", "estilo"]
+
+let personagem = [...hab1,...hab2];
+
+console.log(personagem);
